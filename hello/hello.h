4cee83dd9f3c4d3e5c4c2f49240d4cc3bf8199df
jonghyouk yun <ageldama@gmail.com>
2020-08-13 18:19:02 +0900

    --wip-- [skip ci]

---
+++ hello/hello.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+
+std::string greet(const std::string& name)
+{
+  return "Hello, " + name + "!";
+}
+
--- hello/main.cpp
+++ hello/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <cstdlib>
+#include "hello.h"
 
 using namespace std;
 
 
 int main()
 {
-  cout << "Hello, World!" << endl;
+  cout << greet("World") << endl;
   exit(EXIT_SUCCESS);
 }
