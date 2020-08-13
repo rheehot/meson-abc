50f8aaea79b6fe734f95d55e606e7bd7271eb2a2
jonghyouk yun <ageldama@gmail.com>
2020-08-13 18:32:54 +0900

    --wip-- [skip ci]

---
+++ t_xxhash/greet_test.cpp
@@ -0,0 +1,13 @@
+#include "pch/my_pch.h"
+#include "gtest/gtest.h"
+
+
+TEST(GreetTest, TestGreetWithName) {
+  ASSERT_EQ("Hello, World!", greet("World"));
+}
+
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
